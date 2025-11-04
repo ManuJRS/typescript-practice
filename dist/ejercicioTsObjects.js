69dde00c7c75d86bb2c2b6e746269573f2853cf0
ManuJRS <manueljesusrejonsantana@gmail.com>
2025-11-04 08:54:29 -0600

    New test git TS 4-Nov-2025

---
+++ dist/ejercicioTsObjects.js
@@ -0,0 +1,11 @@
+// 🧩 Ejercicio 1 – Evaluar nivel de experiencia
+export {};
+// Instrucciones:
+// Crea un tipo Developer con propiedades name, yearsOfExperience, language.
+// Luego, crea una función evaluarNivel que devuelva:
+// "Junior" si tiene menos de 2 años.
+// "Mid" si tiene entre 2 y 5.
+// "Senior" si tiene más de 5.
+// Pista:
+// 👉 Usa if...else if...else y asegúrate de tipar correctamente el parámetro y el valor de retorno.
+//# sourceMappingURL=ejercicioTsObjects.js.map
